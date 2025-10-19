3b99e92b688e2be2f4a7e294d2085974359e9fb9
𝙰𝚜𝚑𝚠𝚒𝚗 <aayadav371@gmail.com>
2025-10-19 10:08:46 +0530

    Add files via upload

---
+++ Node Js/hello.js
@@ -0,0 +1,31 @@
+// console.log("hello world!");
+
+// let a = 12;
+// let b = 17;
+
+// console.log(a + b);
+
+// for (let i = 1; i < 100; i++) ;
+// console.log(i);
+
+// let c = 20;
+// let d = 30;
+
+// if (c < d)
+//     console.log("c is greater than d");
+// else (c > d)
+// console.log("c is less than d");
+
+// let e = 10;
+
+// while (e>=1) {
+//     console.log(e);
+//     e--;
+    
+// }
+
+// function hello (a ,b) {
+//     console.log(a + b);
+// }
+// hello(10,20);
+
