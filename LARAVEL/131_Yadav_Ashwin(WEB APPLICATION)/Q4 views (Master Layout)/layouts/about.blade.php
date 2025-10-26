e2b0c36e6ecf102bee7bddddc069706aceb9f593
𝙰𝚜𝚑𝚠𝚒𝚗 <aayadav371@gmail.com>
2025-10-26 20:06:58 +0530

    Delete LARAVEL/131_Yadav_Ashwin(WEB APPLICATION) directory

--- LARAVEL/131_Yadav_Ashwin(WEB APPLICATION)/Q4 views (Master Layout)/layouts/about.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'About Us')
-
-@section('content')
-    <h2>About Us</h2>
-    <p>This is the content of the about page.</p>
-@endsection
--- LARAVEL/131_Yadav_Ashwin(WEB APPLICATION)/Q4 views (Master Layout)/layouts/contact.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Contact Us')
-
-@section('content')
-    <h2>Contact Us</h2>
-    <p>This is the content of the contact page.</p>
-@endsection
